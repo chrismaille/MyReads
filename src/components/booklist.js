97cc9cca27bf63f1def4146465573af0092de1db
Chris Maillefaud <chrismaillefaud@gmail.com>
2018-12-26 13:09:19 -0200

    refactor: MyReads-4
    Create Static Components

---
+++ src/components/booklist.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { Book } from "./book";
+import * as PropTypes from "prop-types";
+
+export class BookList extends Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.title}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map(book => (
+              <Book book={book}/>
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+BookList.propTypes = {
+  title: PropTypes.string,
+  books: PropTypes.any
+};
