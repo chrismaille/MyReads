c5ee21a4ac6bd2d85cb8a0a57a94ac6277e5cd90
Chris Maillefaud <chrismaillefaud@gmail.com>
2019-01-02 16:26:40 -0200

    feat: Add context to Menu Options

--- src/pages/books.js
+++ src/pages/books.js
@@ -4,16 +4,69 @@ import Header from "../components/header";
 import { SearchButton } from "../components/searchbutton";
 import { getAll } from "../BooksAPI";
 import { BookList } from "../components/booklist";
+import Loading from "../components/loading";
 
-const camelCase = require("lodash/camelCase");
+const _ = require("lodash");
+
+const Context = React.createContext({});
+
+export const BookShelfProvider = Context.Provider;
+export const BookShelfConsumer = Context.Consumer;
 
 class BookShelf extends Component {
-  state = { shelfs: [] };
+  state = { shelves: [], loading: true };
+
+  moveToAnotherShelf = ({ bookId, fromShelf, toShelf }) => {
+    // Find current shelves
+    const currentShelf = _.find(
+      this.state.shelves,
+      shelf => fromShelf === _.camelCase(shelf.title)
+    );
+    const destinationShelf = _.find(
+      this.state.shelves,
+      shelf => toShelf === _.camelCase(shelf.title)
+    );
+
+    // Find current book
+    const currentBook = _.find(
+      currentShelf.books,
+      book => book.id === bookId
+    );
+
+    // Update both shelves (move book on one to another)
+    currentShelf.books = currentShelf.books.filter(
+      book => book.id !== bookId
+    );
+    destinationShelf.books.push(currentBook);
+
+    // Recreate shelves array
+    const updatedShelves = this.state.shelves.map(shelf => {
+      if (_.camelCase(shelf.title) === fromShelf) {
+        return {
+          title: shelf.title,
+          books: currentShelf.books
+        };
+      } else if (_.camelCase(shelf.title) === toShelf) {
+        return {
+          title: shelf.title,
+          books: destinationShelf.books
+        };
+      } else {
+        return {
+          title: shelf.title,
+          books: shelf.books
+        };
+      }
+    });
+    // Update state
+    this.setState({ shelves: updatedShelves });
+  };
 
   componentDidMount() {
     getAll().then(books =>
       this.setState({
-        shelfs: [
+        loading: false,
+        shelves: [
           {
             title: "Currently Reading",
             books: books.filter(book => book.shelf === "currentlyReading")
@@ -32,16 +85,28 @@ class BookShelf extends Component {
   }
 
   render() {
+    const contextValue = {
+      data: this.state,
+      handleClick: this.moveToAnotherShelf
+    };
     return (
-      <div className="list-books-content">
-        {this.state.shelfs.map(shelf => (
-          <BookList
-            key={camelCase(shelf.title)}
-            title={shelf.title}
-            books={shelf.books}
-          />
-        ))}
-      </div>
+      <>
+        {this.state.loading ? (
+          <Loading/>
+        ) : (
+          <div className="list-books-content">
+            <BookShelfProvider value={contextValue}>
+              {this.state.shelves.map(shelf => (
+                <BookList
+                  key={_.camelCase(shelf.title)}
+                  title={shelf.title}
+                  books={shelf.books}
+                />
+              ))}
+            </BookShelfProvider>
+          </div>
+        )}
+      </>
     );
   }
 }
